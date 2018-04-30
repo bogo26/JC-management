3893e14baa10e700029c2fbee83a561a228ab778
bogo26 <florin.ionutas@gmail.com>
2018-04-30 22:04:24 +0300

    fix some eshint issues

--- JC vue project/my-project/src/router.js
+++ JC vue project/my-project/src/router.js
@@ -1,21 +1,21 @@
-import Vue from "vue";
-import Router from "vue-router";
-import Workers from "./views/Workers.vue";
-import Jobs from "./views/Jobs.vue";
+import Vue from 'vue';
+import Router from 'vue-router';
+import Workers from './views/Workers.vue';
+import Jobs from './views/Jobs.vue';
 
 Vue.use(Router);
 
 export default new Router({
   routes: [
     {
-      path: "/",
-      name: "workers",
-      component: Workers
+      path: '/',
+      name: 'workers',
+      component: Workers,
     },
     {
-      path: "/jobs",
-      name: "jobs",
-      component: Jobs
-    }
-  ]
+      path: '/jobs',
+      name: 'jobs',
+      component: Jobs,
+    },
+  ],
 });
--- JC vue project/my-project/src/utils/constants.js
+++ JC vue project/my-project/src/utils/constants.js
@@ -1,3 +1,3 @@
-export const API_HOME = "http://localhost:3000";
-export const API_GET_USERS = "/getusers";
-export const API_GET_JOBS = "/getjobs";
+export const API_HOME = 'http://localhost:3000';
+export const API_GET_USERS = '/getusers';
+export const API_GET_JOBS = '/getjobs';
